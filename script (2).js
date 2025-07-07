8a27f50e0a192df836f0d58361f80deb303eca42
cassy43torre <cassytorre43@gmail.com>
2025-07-07 21:29:20 +0800

    Add files via upload

---
+++ script (2).js
@@ -0,0 +1,10 @@
+
+function startSite() {
+  document.getElementById("intro-screen").style.display = "none";
+  document.getElementById("middle-screen").style.display = "block";
+}
+
+function showMainSite() {
+  document.getElementById("middle-screen").style.display = "none";
+  document.getElementById("main-site").style.display = "block";
+}
